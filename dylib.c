262d382d5d84ec29b76ece3da901f54a342b37b7
0xilis <109871561+0xilis@users.noreply.github.com>
2025-03-21 13:51:05 -0400

    LetMePass 1.5

--- dylib.c
+++ dylib.c
@@ -1,22 +1,45 @@
 #include <objc/runtime.h>
+#include <objc/message.h>
 
+__attribute((used)) static id hook_YTVersionUtils_appVersionLong(void) {
+    Class NSStringClass = objc_getClass("NSString");
 
-          /*************************************/          
-        #define zd __attribute__((constructor)) //
-       __attribute((naked, used)) static void/**/a()
-       { /* in-lining asm to prevent nasty compilers
-      from adding useless NOP's and other junk :3c */
-      #ifdef __arm__ /*sadly i cant add more code  */
-      asm ("bx lr;");/*on separate lines after #'s */   
-      #else/*********+:..:+*************************/    
-      asm ("ret;");/****+:    .:=*******************/    
-      #endif/***********+:       .:=****************/  
-      }/****************+:       .-=****************/     
-      zd static void /**+:   ..-+*/ init(){Class cls;
-      SEL name; /*******+:..-+**/ Method me; IMP imp;      
-      /***/cls=objc_getClass("YTUpgradeWorker");/***/
-      /****************************/name=sel_getUid(
-       "startWorkWithCompletionBlock:errorBlock:" );      
-       me=class_getInstanceMethod(cls, name);imp=&a;       
-        method_setImplementation(me,imp);/********/     
-          /************************************/}          
+    SEL allocSelector = sel_getUid("alloc");
+    SEL initSelector = sel_getUid("initWithUTF8String:");
+
+    id stringObject = ((id (*)(Class, SEL))objc_msgSend)(NSStringClass, allocSelector);
+    
+    stringObject = ((id (*)(id, SEL, const char *))objc_msgSend)(stringObject, initSelector, "19.01.1");
+
+    return stringObject;
+}
+
+__attribute((naked, used)) static void hook_YTUpgradeWorker_updateCheck(void) {
+    /* inlining asm to prevent nasty compilers adding useless nops... */
+    #ifdef __arm__
+    asm ("bx lr;");
+    #else
+    asm ("ret;");
+    #endif
+}
+
+__attribute__((constructor)) static void init() {
+    /*
+     * We don't care if the method will ever be implemented in a subclass
+     * because we don't have to worry about YouTube updating their app.
+     * Hence, why IMO using method_setImplementation is fine.
+    */
+    Class cls = objc_getClass("YTUpgradeWorker"); /* wrapper around look_up_class */
+    SEL name = sel_getUid("startWorkWithCompletionBlock:errorBlock:"); /* perhaps this can be done via dyld_get_objc_selector ? */
+    Method meth = class_getInstanceMethod(cls, name);
+    IMP imp = (IMP)&hook_YTUpgradeWorker_updateCheck;
+    method_setImplementation(meth, imp); /* this function already checks meth for nil so this is safe */
+
+    Class versionUtilsClass = objc_getClass("YTVersionUtils");
+    SEL versionSel = sel_getUid("appVersionLong");
+
+    Method versionMethod = class_getClassMethod(versionUtilsClass, versionSel);
+
+    IMP versionImp = (IMP)&hook_YTVersionUtils_appVersionLong;
+    method_setImplementation(versionMethod, versionImp);
+}
